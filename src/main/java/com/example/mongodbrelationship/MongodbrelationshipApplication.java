7d9c175e12b6309ff106aca2654844847881ddf9
Sarita Bendga <Sarita.Bendga@ibm.com>
2021-04-22 17:25:19 +0530

    first commit

---
+++ src/main/java/com/example/mongodbrelationship/MongodbrelationshipApplication.java
@@ -0,0 +1,55 @@
+package com.example.mongodbrelationship;
+
+import com.example.mongodbrelationship.documents.Address;
+import com.example.mongodbrelationship.documents.Hobby;
+import com.example.mongodbrelationship.documents.Person;
+import com.example.mongodbrelationship.repositories.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = PersonRepository.class)
+public class MongodbrelationshipApplication implements CommandLineRunner {
+
+        private PersonRepository personRepository;
+
+    public MongodbrelationshipApplication(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbrelationshipApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        personRepository.deleteAll();
+
+        final Address address = new Address("19 Imaginary Road", "Imaginary Place", "Imaginary City", "UK");
+
+        final Hobby badminton = new Hobby("Badminton");
+        final Hobby tv = new Hobby("TV");
+        final List<Hobby> hobbies = Arrays.asList(badminton, tv);
+
+        final Person john = new Person("John", "Doe", LocalDateTime.now(), address, "Winner", 100, hobbies);
+        personRepository.save(john);
+
+        System.out.println("Find by first name");
+        personRepository.findByFirstName("John").forEach(System.out::println);
+
+        System.out.println("Find by country (UK)");
+        personRepository.findByCountry("UK").forEach(System.out::println);
+
+        address.setCountry("US");
+        personRepository.save(john);
+        System.out.println("Find by country (US)");
+        personRepository.findByCountry("US").forEach(System.out::println);
+    }
+
+
+}
---
+++ src/main/java/com/example/mongodbrelationship/repositories/PersonRepository.java
@@ -0,0 +1,15 @@
+package com.example.mongodbrelationship.repositories;
+
+import com.example.mongodbrelationship.documents.Person;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface PersonRepository extends MongoRepository<Person, String> {
+
+    List<Person> findByFirstName(final String firstName);
+
+    @Query("{'address.country': ?0}")
+    List<Person> findByCountry(final String country);
+}
